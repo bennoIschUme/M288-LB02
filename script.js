93fc10740067152c53b7e94f4484ac0be5cbb80a
julian-49 <103259817+julian-49@users.noreply.github.com>
2023-01-14 15:12:55 +0100

    v3

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-const form = document.querySelector("form");
-eField = form.querySelector(".email"),
-    eInput = eField.querySelector("input"),
-    pField = form.querySelector(".password"),
-    pInput = pField.querySelector("input");
-form.onsubmit = (e)=>{
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
 
-    (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
-    (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
-    setTimeout(()=>{
-        eField.classList.remove("shake");
-        pField.classList.remove("shake");
-    }, 500);
-    eInput.onkeyup = ()=>{checkEmail();}
-    pInput.onkeyup = ()=>{checkPass();}
-    function checkEmail(){
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-        if(!eInput.value.match(pattern)){
-            eField.classList.add("error");
-            eField.classList.remove("valid");
-            let errorTxt = eField.querySelector(".error-txt");
-
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
-        }else{
-            eField.classList.remove("error");
-            eField.classList.add("valid");
-        }
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
     }
-    function checkPass(){
-        if(pInput.value == ""){
-            pField.classList.add("error");
-            pField.classList.remove("valid");
-        }else{
-            pField.classList.remove("error");
-            pField.classList.add("valid");
-        }
-    }
-    if(!eField.classList.contains("error") && !pField.classList.contains("error")){
-        window.location.href = form.getAttribute("action");
-    }
-}
+})
