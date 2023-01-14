964057c5cf96c32303d5f2ee027adc974fb9067c
bennoIschUme <niederlaenderb@bzz.ch>
2023-01-14 15:45:52 +0100

    v7

--- script.js
+++ script.js
@@ -2,20 +2,14 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-}
-
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (loginSuccessful) {
-        window.open("http://localhost:63342/M288-LB02/home.html?_ijt=mjvjg11a1bhnckf3hod65kfmvj&_ij_reload=RELOAD_ON_SAVE", "_blank");
-    }
-
     if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
