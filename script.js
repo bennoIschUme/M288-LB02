12c31356898c218d0e25a1c1c96216f37bc673ec
bennoIschUme <niederlaenderb@bzz.ch>
2023-01-17 21:57:05 +0100

    v18

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user@gmail.com" && password === "Bzz_Ict12345") {
+    if (username === "user@gmail.com" && password === "Bzz_ict12345") {
         loginButton.disabled = false;
         window.location = 'home.html', "_blank";
     } else {
