f00e6f2a25f12332ff10cc7c0b9fe3e0443eec8c
julian-49 <103259817+julian-49@users.noreply.github.com>
2023-01-17 21:10:05 +0100

    v26

--- script.js
+++ script.js
@@ -2,18 +2,33 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+<!-- Hier wird die Json-Datei eingebunden!!! -->
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+
+fetch('json-datei.json')
+    .then(response => response.json())
+    .then(data => {
+
+        <!-- if & else prüfen Passwort  -->
+        const username = loginForm.username.value;
+        const password = loginForm.password.value;
+
+        if(data.username === username && data.password === password){
+            loginButton.disabled = false;
+            window.location = 'home.html', "_blank";
+        }else{
+            loginButton.disabled = true;
+            loginErrorMsg.style.opacity = 1;
+        }
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user@gmail.com" && password === "Bzz_Ict12345") {
-        loginButton.disabled = false;
-        window.location = 'home.html', "_blank";
-    } else {
-        loginButton.disabled = true;
-        loginErrorMsg.style.opacity = 1;
-    }
-
-
-})
+});
